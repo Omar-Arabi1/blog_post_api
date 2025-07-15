278f7adf09b8f00a702427da12f02bf50640d5f0
Omar-Arabi1 <omarbuisness7@gmail.com>
2025-07-15 20:42:45 +0300

    added an endpoint to view all the comments to a certain post

--- post_actions/post_actions.py
+++ post_actions/post_actions.py
@@ -14,6 +14,22 @@ router = APIRouter(
 
 COMMENT_MAX_WORD_COUNT: int = 100
 
+@router.get('/view_comments/{post_id}')
+async def view_comments(post_id: str, db: db_dependency, user: user_dependency) -> None:
+    check_logged_in(user=user)
+    
+    post: Post = db.query(Post).filter(Post.id == post_id).first()
+    
+    if post is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail='could not find post'
+        )
+    
+    post_comments = db.query(Comment).filter(Comment.mother_post_id == post.id).all()
+    
+    return {'post_comments': post_comments}
+
 @router.post('/{post_id}')
 async def create_comment(post_id: str, db: db_dependency, user: user_dependency, comment_body: str) -> None:
     check_logged_in(user=user)
@@ -26,16 +42,10 @@ async def create_comment(post_id: str, db: db_dependency, user: user_dependency,
             detail='count not find the post'
         )
 
-    if is_empty(comment_body) is True:
+    if is_empty(comment_body) is True or len(comment_body.split()) > COMMENT_MAX_WORD_COUNT:
         raise HTTPException(
             status_code=status.HTTP_406_NOT_ACCEPTABLE,
-            detail='can not accept an empty comment'
-        )
-
-    if len(comment_body.split()) > COMMENT_MAX_WORD_COUNT:
-        raise HTTPException(
-            status_code=status.HTTP_406_NOT_ACCEPTABLE,
-            detail='can not accept a comment that passes the max word count'
+            detail='can not accept comment'
         )
 
     comment: Comment = Comment(
@@ -56,4 +66,3 @@ async def create_comment(post_id: str, db: db_dependency, user: user_dependency,
     db.commit()
     
     return {"comment": serializable_comment}
-    
