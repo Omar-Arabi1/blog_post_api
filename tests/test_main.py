1dc6a8296082b00b21f86bdfc5d05217fcedc949
Omar-Arabi1 <omarbuisness7@gmail.com>
2025-07-16 16:10:26 +0300

    added more indepth tests for listing the posts for wether its full or it has contents in it

--- tests/test_main.py
+++ tests/test_main.py
@@ -26,11 +26,29 @@ def setup_function() -> None:
     db.add(mock_user)
     db.commit()
 
-def test_list_posts() -> None:
+def test_list_posts_if_posts_is_empty() -> None:
     list_posts_response: Response = api_requests_helper.list_posts_request()
 
     assert list_posts_response.status_code == 200
     assert list_posts_response.json().get('posts') == []
+
+def test_list_posts_if_posts_is_full() -> None:
+    test_titles: list[str] = ['first test title', 'second test title', 'third test title']
+    for index, title in enumerate(test_titles):
+        add_post_response: Response = api_requests_helper.add_post_request(post_data=test_post_data, title=title)
+
+        assert add_post_response.status_code == 200
+
+        list_posts_response: Response = api_requests_helper.list_posts_request()
+        post_creator_id: str = list_posts_response.json().get('posts')[index].get('creator_id')
+        post_title: str = list_posts_response.json().get('posts')[index].get('title')
+        post_data: str = list_posts_response.json().get('posts')[index].get('post_data')
+
+
+        assert list_posts_response.status_code == 200
+        assert post_creator_id == 'test_id'
+        assert post_title == title
+        assert post_data == test_post_data
 
 def test_launch_post() -> None:
     launch_post_response: Response = api_requests_helper.add_post_request(post_data=test_post_data, title=test_post_title)
@@ -43,11 +61,11 @@ def test_updated_post() -> None:
     assert launch_post_response.status_code == 200
 
     post_id: str = launch_post_response.json().get('post').get('id')
-    
+
     updated_title: str = 'fake_updated_title'
 
     updated_post_response: Response = api_requests_helper.update_post_request(updated_title=updated_title, post_id=post_id)
-    
+
     assert updated_post_response.status_code == 200
 
 def test_delete_post() -> None:
@@ -58,6 +76,6 @@ def test_delete_post() -> None:
     post_id: str = launch_post_response.json().get('post').get('id')
 
     delete_post_response: Response = api_requests_helper.delete_post_request(post_id=post_id)
-    
+
     assert delete_post_response.status_code == 200
     assert delete_post_response.json().get('deleted_post') == launch_post_response.json().get('post')
